d87a2e305a2f4f999e5a5a4da4794fca399eda27
poly000 <34085039+poly000@users.noreply.github.com>
2019-02-20 04:51:19 +0800

    Add files via upload

--- doujinshi-compress2jpg.sh
+++ doujinshi-compress2jpg.sh
@@ -1,56 +1,35 @@
-#!/usr/bin/bash
-:<<QWQ
-needs ffmpeg rename and libraries.
-工作目录： 目录下有多个文件夹，其中皆为本子。没有*txt之类的东西
-Thumb.db会自动删除。
-适合本子压制组使用
-POWERED BY PE
-QWQ
-find -name *db -delete
+#!/bin/bash
 
-while	ls *\ * &> /dev/null
-do	rename \  空__格 * &> /dev/null
-done
+perl-rename 's\ \空__格\g' *
 
 for N in	`find -type d|grep /`
-do	cd "$N"
-	while	ls *\ * &> /dev/null
-	do	rename \  空__格 * &> /dev/null
-	done
-
+do	cd ${N}
+	perl-rename 's\ \空__格\g' *
 	cd ../
 done
 
 for a in	`find -type f`
-do	ffmpeg -v 0 -i "$a" "$a.jpg"
-	rm "$a"
+do	ffmpeg -v 0 -i ${a} ${a}.jpg
+	rm ${a}
 done
 
-a=
-
 for N in	`find -type d|grep /`
-do	cd "$N"
+do	cd ${N}
 	b=
-
 	for a in	`ls`
 	do	b=$((b+1))
 		mv $a P$b.jpg
 	done
-
-	rename P P00 P?.jpg &> /dev/null
-	rename P P0 P??.jpg &> /dev/null
-
-	for a in `ls`
-	do	mv $a ${a#P*}
-	done
-
+	
+	rename P P00 P?.jpg
+	rename P P0 P??.jpg
+	
+	perl-rename 's\P\\g' *
+	
 	cd ../
 done
-N=;a=;b=
 
-while	ls *空__格* &> /dev/null
-do	rename 空__格 \  * &> /dev/null
-done
+N=;a=;b=
+perl-rename 's\空__格\ \g' *
 
 echo Done!
-
