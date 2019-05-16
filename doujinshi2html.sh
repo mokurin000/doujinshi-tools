34ef603ff05e8e47513622be5854b6ee30da6e8f
poly000 <34085039+poly000@users.noreply.github.com>
2019-05-16 22:44:34 +0800

    Update doujinshi2html.sh

--- doujinshi2html.sh
+++ doujinshi2html.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/bash
 if [ "$1"x != convertx ]
-then	echo Usage: $0 convert [filename.html]
-	echo works in：a directory with some images
-	exit 0
+	then	echo Usage: $0 convert [filename.html]
+		echo works in：a directory with some images
+		exit 0
 fi
-
 if [ "$2"x != x ]
-then	OPFILE=$2
-else	OPFILE=output.html
+	then	OPFILE=$2
+	else	OPFILE=output.html
 fi
-
 echo -e -n '<!doctype html><html>\n<head>\n<meta charset="utf-8">\n</head>'"<body>" > $OPFILE
-
 for var in `find . -name "*[Gg]"` `find . -name "*[Pp]"`
 do	case $var in
 	*.[Pp][Nn][Gg])
@@ -31,6 +28,4 @@ do	case $var in
 	base64 $var >> $OPFILE
 	echo -e '">\n</img>' >> $OPFILE
 done
-
 echo -n '</body></html>' >> $OPFILE
-
