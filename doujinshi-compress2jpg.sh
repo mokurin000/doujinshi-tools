f98954c76ae2711ff2090e80ab92b6ddb46bf9af
poly000 <34085039+poly000@users.noreply.github.com>
2019-05-16 22:44:00 +0800

    Update doujinshi-compress2jpg.sh

--- doujinshi-compress2jpg.sh
+++ doujinshi-compress2jpg.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-
 IFS=$'\n'
-
 for a in	`find -type f`
 do	ffmpeg -v 0 -i ${a} ${a}.jpg
 	rm ${a}
 done
-
 for N in	`find -type d|grep /`
 do	cd ${N}
-	b=
+	typeset -i b=0
 	for a in	`ls`
 	do	b=$((b+1))
 		mv $a P$b.jpg
 	done
-	
 	rename P P00 P?.jpg
 	rename P P0 P??.jpg
-	
 	perl-rename 's\P\\g' *
-	
 	cd ../
 done
-
 echo Done!
